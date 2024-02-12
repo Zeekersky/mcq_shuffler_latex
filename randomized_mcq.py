eb66b16a2f550278aa9a76f3d606a4053484a17c
Zeekersky <pal82087@gmail.com>
2024-02-12 22:33:30 +0530

    comment line argument added and footer changed

--- randomized_mcq.py
+++ randomized_mcq.py
@@ -2,6 +2,8 @@ import re
 import random
 import itertools
 import os
+import sys
+import string
 
 # Read Latex code from a file
 def read(file):
@@ -15,6 +17,9 @@ input_latex_name = 'quiz2.tex'
 output_latex_path = 'modified'
 
 #_______________________________________________________________________________________
+
+num_mcq = int(sys.argv[1])
+num_permutations = int(sys.argv[2])
 
 latex_code = read(os.path.join(input_latex_path, input_latex_name))
 
@@ -35,7 +40,7 @@ if len(matches) >= 2:
     items = re.findall(pattern_items, part2)
     remaining_part = part2.split(r'\end{enumerate*}')[-1]
 
-    num_mcq = int(input('Enter the number of MCQs to generate: '))
+    # num_mcq = int(input('Enter the number of MCQs to generate: '))
 
     for item in items[:-1]:
         questions.append('\\item' + item + r'\end{enumerate*}')
@@ -51,10 +56,13 @@ if len(matches) >= 2:
 
     all_permutations = list(itertools.permutations(questions_need_shuffle))
     print(f'Total number of different set of questions (upto {len(all_permutations)}) to generate: ', end="")
-    num_permutations = int(input())
+    # num_permutations = int(input())
     selected_permutations = random.sample(all_permutations, num_permutations)
+    set_number = 0
 
     for i, mcq_questions in enumerate(selected_permutations):
+
+        set_number += 1
 
         questions_choice_shuffle = []
         for question in mcq_questions:
@@ -81,9 +89,13 @@ if len(matches) >= 2:
 
         shuffled_questions = '\n\n'.join(questions_choice_shuffle)
         reassembled_latex = part1 + '\n' + r'\begin{enumerate}' + shuffled_questions + '\n' + '\n'.join(questions_not_need_shuffle) + '\n' + remaining_part + part3
-        # print(reassembled_latex)
+
+        pattern_set = re.compile(r'footnote{(.*?)}', re.DOTALL)
+        set_found = re.findall(pattern_set, reassembled_latex)
+        random_characters = [random.choice(string.ascii_letters) for _ in range(len(set_found[0])-1)]
+        if set_found:
+            reassembled_latex = re.sub(pattern_set, r'footnote{' + str(set_number) + ''.join(random_characters) + '}' , reassembled_latex, count=1)
+
         output_file = f'shuffled_quiz_{i + 1}.tex'
         with open(os.path.join(output_latex_path, output_file), 'w', encoding='utf-8') as output_file:
-            output_file.write(reassembled_latex)    
-
-    
+            output_file.write(reassembled_latex)
